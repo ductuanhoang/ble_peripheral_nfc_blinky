a930403118eeafd0a8197c3d7d684946c3be3672
tuanhd <gianghohuynhde93@gmail.com>
2022-09-09 20:32:36 +0700

    update control led via nordic blinky app

--- src/main.c
+++ src/main.c
@@ -22,6 +22,7 @@
 #include <nfc/ndef/le_oob_rec.h>
 #include <nfc/ndef/le_oob_rec_parser.h>
 
+#include <bluetooth/services/lbs.h>
 #include <nfc/tnep/tag.h>
 #include <nfc/tnep/ch.h>
 
@@ -37,6 +38,7 @@
 
 #define NFC_FIELD_LED DK_LED2
 #define CON_STATUS_LED DK_LED1
+#define USER_LED DK_LED3
 
 #define KEY_BOND_REMOVE_MASK DK_BTN4_MSK
 
@@ -52,13 +54,22 @@ static struct bt_le_oob oob_remote;
 
 /* Bonded address queue. */
 K_MSGQ_DEFINE(bonds_queue,
-	      sizeof(bt_addr_le_t),
-	      CONFIG_BT_MAX_PAIRED,
-	      4);
+			  sizeof(bt_addr_le_t),
+			  CONFIG_BT_MAX_PAIRED,
+			  4);
+
+// static const struct bt_data ad[] = {
+// 	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+// 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+// };
 
 static const struct bt_data ad[] = {
-	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
+
+static const struct bt_data sd[] = {
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_LBS_VAL),
 };
 
 static struct k_poll_signal pair_signal;
@@ -68,12 +79,22 @@ static uint8_t tnep_swap_buffer[NFC_TNEP_BUFFER_SIZE];
 static bool use_remote_tk;
 static bool adv_permission;
 
+static void app_led_cb(bool led_state);
+static bool app_button_cb(void);
+
+static struct bt_lbs_cb lbs_callbacs = {
+	.led_cb    = app_led_cb,
+	.button_cb = app_button_cb,
+};
+
+
 static int tk_value_generate(void)
 {
 	int err;
 
 	err = bt_rand(tk_value, sizeof(tk_value));
-	if (err) {
+	if (err)
+	{
 		printk("Random TK value generation failed: %d\n", err);
 	}
 
@@ -84,8 +105,8 @@ static void pair_key_generate_init(void)
 {
 	k_poll_signal_init(&pair_signal);
 	k_poll_event_init(&events[NFC_TNEP_EVENTS_NUMBER],
-			  K_POLL_TYPE_SIGNAL, K_POLL_MODE_NOTIFY_ONLY,
-			  &pair_signal);
+					  K_POLL_TYPE_SIGNAL, K_POLL_MODE_NOTIFY_ONLY,
+					  &pair_signal);
 }
 
 static int paring_key_generate(void)
@@ -95,7 +116,8 @@ static int paring_key_generate(void)
 	printk("Generating new pairing keys\n");
 
 	err = bt_le_oob_get_local(BT_ID_DEFAULT, &oob_local);
-	if (err) {
+	if (err)
+	{
 		printk("Error while fetching local OOB data: %d\n", err);
 	}
 
@@ -106,9 +128,11 @@ static void paring_key_process(void)
 {
 	int err;
 
-	if (events[NFC_TNEP_EVENTS_NUMBER].state == K_POLL_STATE_SIGNALED) {
+	if (events[NFC_TNEP_EVENTS_NUMBER].state == K_POLL_STATE_SIGNALED)
+	{
 		err = paring_key_generate();
-		if (err) {
+		if (err)
+		{
 			printk("Pairing key generation error: %d\n", err);
 		}
 
@@ -124,13 +148,15 @@ static void bond_find(const struct bt_bond_info *info, void *user_data)
 
 	/* Filter already connected peers. */
 	conn = bt_conn_lookup_addr_le(BT_ID_DEFAULT, &info->addr);
-	if (conn) {
+	if (conn)
+	{
 		bt_conn_unref(conn);
 		return;
 	}
 
-	err = k_msgq_put(&bonds_queue, (void *) &info->addr, K_NO_WAIT);
-	if (err) {
+	err = k_msgq_put(&bonds_queue, (void *)&info->addr, K_NO_WAIT);
+	if (err)
+	{
 		printk("No space in the queue for the bond\n");
 	}
 }
@@ -147,16 +173,17 @@ static void advertising_start(void)
  * @brief Callback function for handling NFC events.
  */
 static void nfc_callback(void *context,
-			 nfc_t4t_event_t event,
-			 const uint8_t *data,
-			 size_t data_length,
-			 uint32_t flags)
+						 nfc_t4t_event_t event,
+						 const uint8_t *data,
+						 size_t data_length,
+						 uint32_t flags)
 {
 	ARG_UNUSED(context);
 	ARG_UNUSED(data);
 	ARG_UNUSED(flags);
 
-	switch (event) {
+	switch (event)
+	{
 	case NFC_T4T_EVENT_FIELD_ON:
 		nfc_tnep_tag_on_selected();
 		dk_set_led_on(NFC_FIELD_LED);
@@ -171,7 +198,8 @@ static void nfc_callback(void *context,
 		break;
 
 	case NFC_T4T_EVENT_NDEF_READ:
-		if (adv_permission) {
+		if (adv_permission)
+		{
 			advertising_start();
 			adv_permission = false;
 		}
@@ -179,9 +207,10 @@ static void nfc_callback(void *context,
 		break;
 
 	case NFC_T4T_EVENT_NDEF_UPDATED:
-		if (data_length > 0) {
+		if (data_length > 0)
+		{
 			nfc_tnep_tag_rx_msg_indicate(nfc_t4t_ndef_file_msg_get(data),
-						     data_length);
+										 data_length);
 		}
 
 	default:
@@ -189,39 +218,61 @@ static void nfc_callback(void *context,
 	}
 }
 
+static void app_led_cb(bool led_state)
+{
+	dk_set_led(USER_LED, led_state);
+}
+
+static bool app_button_cb(void)
+{
+	return 0;
+}
+
 static void advertising_continue(void)
 {
 	struct bt_le_adv_param adv_param;
 
 	bt_addr_le_t addr;
-
-	if (!k_msgq_get(&bonds_queue, &addr, K_NO_WAIT)) {
+	// user init blinking services
+	int err = bt_lbs_init(&lbs_callbacs);
+	if (err)
+	{
+		printk("Failed to init LBS (err:%d)\n", err);
+		return;
+	}
+	// end
+	if (!k_msgq_get(&bonds_queue, &addr, K_NO_WAIT))
+	{
 		char addr_buf[BT_ADDR_LE_STR_LEN];
 
 		adv_param = *BT_LE_ADV_CONN_DIR(&addr);
 		adv_param.options |= BT_LE_ADV_OPT_DIR_ADDR_RPA;
 
-		int err = bt_le_adv_start(&adv_param, NULL, 0, NULL, 0);
+		int err = bt_le_adv_start(&adv_param, NULL, 0, sd, ARRAY_SIZE(sd));
 
-		if (err) {
+		if (err)
+		{
 			printk("Directed advertising failed to start\n");
 			return;
 		}
 
 		bt_addr_le_to_str(&addr, addr_buf, BT_ADDR_LE_STR_LEN);
 		printk("Direct advertising to %s started\n", addr_buf);
-	} else {
+	}
+	else
+	{
 		int err;
 
 		adv_param = *BT_LE_ADV_CONN;
 		adv_param.options |= BT_LE_ADV_OPT_ONE_TIME;
 		err = bt_le_adv_start(&adv_param, ad, ARRAY_SIZE(ad),
-				      NULL, 0);
+							  sd, ARRAY_SIZE(sd));
 
 		/* Re-enabling advertising when it is already active
 		 * is not an error here.
 		 */
-		if (err && (err != -EALREADY)) {
+		if (err && (err != -EALREADY))
+		{
 			printk("Advertising failed to start (err %d)\n", err);
 			return;
 		}
@@ -245,28 +296,30 @@ static void auth_cancel(struct bt_conn *conn)
 }
 
 static void lesc_oob_data_set(struct bt_conn *conn,
-			      struct bt_conn_oob_info *oob_info)
+							  struct bt_conn_oob_info *oob_info)
 {
 	int err;
 	char addr[BT_ADDR_LE_STR_LEN];
 	struct bt_conn_info info;
 
 	err = bt_conn_get_info(conn, &info);
-	if (err) {
+	if (err)
+	{
 		return;
 	}
 
 	struct bt_le_oob_sc_data *oob_data_local =
 		oob_info->lesc.oob_config != BT_CONN_OOB_REMOTE_ONLY
-					  ? &oob_local.le_sc_data
-					  : NULL;
+			? &oob_local.le_sc_data
+			: NULL;
 	struct bt_le_oob_sc_data *oob_data_remote =
 		oob_info->lesc.oob_config != BT_CONN_OOB_LOCAL_ONLY
-					  ? &oob_remote.le_sc_data
-					  : NULL;
+			? &oob_remote.le_sc_data
+			: NULL;
 
 	if (oob_data_remote &&
-	    bt_addr_le_cmp(info.le.remote, &oob_remote.addr)) {
+		bt_addr_le_cmp(info.le.remote, &oob_remote.addr))
+	{
 		bt_addr_le_to_str(info.le.remote, addr, sizeof(addr));
 		printk("No OOB data available for remote %s", addr);
 		bt_conn_auth_cancel(conn);
@@ -274,7 +327,8 @@ static void lesc_oob_data_set(struct bt_conn *conn,
 	}
 
 	if (oob_data_local &&
-	    bt_addr_le_cmp(info.le.local, &oob_local.addr)) {
+		bt_addr_le_cmp(info.le.local, &oob_local.addr))
+	{
 		bt_addr_le_to_str(info.le.local, addr, sizeof(addr));
 		printk("No OOB data available for local %s", addr);
 		bt_conn_auth_cancel(conn);
@@ -282,7 +336,8 @@ static void lesc_oob_data_set(struct bt_conn *conn,
 	}
 
 	err = bt_le_oob_set_sc_data(conn, oob_data_local, oob_data_remote);
-	if (err) {
+	if (err)
+	{
 		printk("Error while setting OOB data: %d\n", err);
 	}
 }
@@ -293,7 +348,8 @@ static void legacy_tk_value_set(struct bt_conn *conn)
 	const uint8_t *tk = use_remote_tk ? remote_tk_value : tk_value;
 
 	err = bt_le_oob_set_legacy_tk(conn, tk);
-	if (err) {
+	if (err)
+	{
 		printk("TK value set error: %d\n", err);
 	}
 
@@ -301,14 +357,16 @@ static void legacy_tk_value_set(struct bt_conn *conn)
 }
 
 static void auth_oob_data_request(struct bt_conn *conn,
-				  struct bt_conn_oob_info *info)
+								  struct bt_conn_oob_info *info)
 {
-	if (info->type == BT_CONN_OOB_LE_SC) {
+	if (info->type == BT_CONN_OOB_LE_SC)
+	{
 		printk("LESC OOB data requested\n");
 		lesc_oob_data_set(conn, info);
 	}
 
-	if (info->type == BT_CONN_OOB_LE_LEGACY) {
+	if (info->type == BT_CONN_OOB_LE_LEGACY)
+	{
 		printk("Legacy TK value requested\n");
 		legacy_tk_value_set(conn);
 	}
@@ -339,14 +397,14 @@ static void pairing_failed(struct bt_conn *conn, enum bt_security_err reason)
 }
 
 static enum bt_security_err pairing_accept(struct bt_conn *conn,
-				const struct bt_conn_pairing_feat *const feat)
+										   const struct bt_conn_pairing_feat *const feat)
 {
-	if (feat->oob_data_flag && (!(feat->auth_req & AUTH_SC_FLAG))) {
+	if (feat->oob_data_flag && (!(feat->auth_req & AUTH_SC_FLAG)))
+	{
 		bt_set_oob_data_flag(true);
 	}
 
 	return BT_SECURITY_ERR_SUCCESS;
-
 }
 
 static struct bt_conn_auth_cb conn_auth_callbacks = {
@@ -357,8 +415,7 @@ static struct bt_conn_auth_cb conn_auth_callbacks = {
 
 static struct bt_conn_auth_info_cb conn_auth_info_callbacks = {
 	.pairing_complete = pairing_complete,
-	.pairing_failed = pairing_failed
-};
+	.pairing_failed = pairing_failed};
 
 static void connected(struct bt_conn *conn, uint8_t err)
 {
@@ -366,11 +423,15 @@ static void connected(struct bt_conn *conn, uint8_t err)
 
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
-	if (err) {
-		if (err == BT_HCI_ERR_ADV_TIMEOUT) {
+	if (err)
+	{
+		if (err == BT_HCI_ERR_ADV_TIMEOUT)
+		{
 			printk("Direct advertising to %s timed out\n", addr);
 			k_work_submit(&adv_work);
-		} else {
+		}
+		else
+		{
 			printk("Failed to connect to %s (%u)\n", addr, err);
 		}
 
@@ -382,7 +443,8 @@ static void connected(struct bt_conn *conn, uint8_t err)
 	printk("Connected %s\n", addr);
 	dk_set_led_on(CON_STATUS_LED);
 
-	if (conn_cnt < CONFIG_BT_MAX_CONN) {
+	if (conn_cnt < CONFIG_BT_MAX_CONN)
+	{
 		advertising_start();
 	}
 }
@@ -395,7 +457,8 @@ static void disconnected(struct bt_conn *conn, uint8_t reason)
 
 	conn_cnt--;
 
-	if (!conn_cnt) {
+	if (!conn_cnt)
+	{
 		dk_set_led_off(CON_STATUS_LED);
 	}
 
@@ -403,17 +466,20 @@ static void disconnected(struct bt_conn *conn, uint8_t reason)
 }
 
 static void security_changed(struct bt_conn *conn, bt_security_t level,
-			     enum bt_security_err err)
+							 enum bt_security_err err)
 {
 	char addr[BT_ADDR_LE_STR_LEN];
 
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
-	if (!err) {
+	if (!err)
+	{
 		printk("Security changed: %s level %u\n", addr, level);
-	} else {
+	}
+	else
+	{
 		printk("Security failed: %s level %u err %d\n", addr, level,
-			err);
+			   err);
 	}
 }
 
@@ -422,7 +488,6 @@ BT_CONN_CB_DEFINE(conn_callbacks) = {
 	.disconnected = disconnected,
 	.security_changed = security_changed,
 };
-
 
 /** .. include_startingpoint_pair_msg_rst */
 static int tnep_initial_msg_encode(struct nfc_ndef_msg_desc *msg)
@@ -434,7 +499,7 @@ static int tnep_initial_msg_encode(struct nfc_ndef_msg_desc *msg)
 	NFC_NDEF_LE_OOB_RECORD_DESC_DEF(oob_rec, '0', &rec_payload);
 	NFC_NDEF_CH_AC_RECORD_DESC_DEF(oob_ac, NFC_AC_CPS_ACTIVE, 1, "0", 0);
 	NFC_NDEF_CH_HS_RECORD_DESC_DEF(hs_rec, NFC_NDEF_CH_MSG_MAJOR_VER,
-				       NFC_NDEF_CH_MSG_MINOR_VER, 1);
+								   NFC_NDEF_CH_MSG_MINOR_VER, 1);
 
 	memset(&rec_payload, 0, sizeof(rec_payload));
 
@@ -452,10 +517,11 @@ static int tnep_initial_msg_encode(struct nfc_ndef_msg_desc *msg)
 	ch_records.carrier = &NFC_NDEF_LE_OOB_RECORD_DESC(oob_rec);
 	ch_records.cnt = 1;
 
-	err =  nfc_ndef_ch_msg_hs_create(msg,
-					 &NFC_NDEF_CH_RECORD_DESC(hs_rec),
-					 &ch_records);
-	if (err) {
+	err = nfc_ndef_ch_msg_hs_create(msg,
+									&NFC_NDEF_CH_RECORD_DESC(hs_rec),
+									&ch_records);
+	if (err)
+	{
 		return err;
 	}
 
@@ -464,39 +530,46 @@ static int tnep_initial_msg_encode(struct nfc_ndef_msg_desc *msg)
 /** .. include_endpoint_pair_msg_rst */
 
 static int check_oob_carrier(const struct nfc_tnep_ch_record *ch_record,
-			     const struct nfc_ndef_record_desc **oob_data)
+							 const struct nfc_ndef_record_desc **oob_data)
 {
 	const struct nfc_ndef_ch_ac_rec *ac_rec = NULL;
 
-	for (size_t i = 0; i < ch_record->count; i++) {
-		if (nfc_ndef_le_oob_rec_check(ch_record->carrier[i])) {
+	for (size_t i = 0; i < ch_record->count; i++)
+	{
+		if (nfc_ndef_le_oob_rec_check(ch_record->carrier[i]))
+		{
 			*oob_data = ch_record->carrier[i];
 		}
 	}
 
-	if (!oob_data) {
+	if (!oob_data)
+	{
 		printk("Connection Handover Requester not supporting OOB BLE\n");
 		return -EINVAL;
 	}
 
 	/* Look for the corresponding Alternative Carrier Record. */
-	for (size_t i = 0; i < ch_record->count; i++) {
+	for (size_t i = 0; i < ch_record->count; i++)
+	{
 		if (((*oob_data)->id_length == ch_record->ac[i].carrier_data_ref.length) &&
-		    (memcmp((*oob_data)->id,
-			    ch_record->ac[i].carrier_data_ref.data,
-			    (*oob_data)->id_length) == 0)) {
+			(memcmp((*oob_data)->id,
+					ch_record->ac[i].carrier_data_ref.data,
+					(*oob_data)->id_length) == 0))
+		{
 			ac_rec = &ch_record->ac[i];
 		}
 	}
 
-	if (!ac_rec) {
+	if (!ac_rec)
+	{
 		printk("No Alternative Carrier Record for OOB LE carrier\n");
 		return -EINVAL;
 	}
 
 	/* Check carrier state */
 	if ((ac_rec->cps != NFC_AC_CPS_ACTIVE) &&
-	    (ac_rec->cps != NFC_AC_CPS_ACTIVATING)) {
+		(ac_rec->cps != NFC_AC_CPS_ACTIVATING))
+	{
 		printk("LE OBB Carrier inactive\n");
 		return -EINVAL;
 	}
@@ -505,7 +578,7 @@ static int check_oob_carrier(const struct nfc_tnep_ch_record *ch_record,
 }
 
 static int oob_le_data_handle(const struct nfc_ndef_record_desc *rec,
-			      bool request)
+							  bool request)
 {
 	int err;
 	const struct nfc_ndef_le_oob_rec_payload_desc *oob;
@@ -513,33 +586,38 @@ static int oob_le_data_handle(const struct nfc_ndef_record_desc *rec,
 	uint32_t desc_buf_len = sizeof(desc_buf);
 
 	err = nfc_ndef_le_oob_rec_parse(rec, desc_buf,
-					&desc_buf_len);
-	if (err) {
+									&desc_buf_len);
+	if (err)
+	{
 		printk("Error during NDEF LE OOB Record parsing, err: %d.\n",
-			err);
+			   err);
 	}
 
-	oob = (struct nfc_ndef_le_oob_rec_payload_desc *) desc_buf;
+	oob = (struct nfc_ndef_le_oob_rec_payload_desc *)desc_buf;
 
 	nfc_ndef_le_oob_rec_printout(oob);
 
 	if ((*oob->le_role != NFC_NDEF_LE_OOB_REC_LE_ROLE_CENTRAL_ONLY) &&
-	    (*oob->le_role != NFC_NDEF_LE_OOB_REC_LE_ROLE_CENTRAL_PREFFERED)) {
+		(*oob->le_role != NFC_NDEF_LE_OOB_REC_LE_ROLE_CENTRAL_PREFFERED))
+	{
 		printk("Unsupported Device LE Role\n");
 		return -EINVAL;
 	}
 
-	if (oob->le_sc_data || oob->tk_value) {
+	if (oob->le_sc_data || oob->tk_value)
+	{
 		bt_set_oob_data_flag(true);
 	}
 
-	if (oob->le_sc_data) {
+	if (oob->le_sc_data)
+	{
 
 		oob_remote.le_sc_data = *oob->le_sc_data;
 		bt_addr_le_copy(&oob_remote.addr, oob->addr);
 	}
 
-	if (oob->tk_value) {
+	if (oob->tk_value)
+	{
 		memcpy(remote_tk_value, oob->tk_value, sizeof(remote_tk_value));
 		use_remote_tk = request;
 	}
@@ -570,8 +648,8 @@ static int carrier_prepare(void)
 	rec_payload.flags = NFC_NDEF_LE_OOB_REC_FLAGS(BT_LE_AD_NO_BREDR);
 
 	return nfc_tnep_ch_carrier_set(&NFC_NDEF_CH_AC_RECORD_DESC(oob_ac),
-				       &NFC_NDEF_LE_OOB_RECORD_DESC(oob_rec),
-				       1);
+								   &NFC_NDEF_LE_OOB_RECORD_DESC(oob_rec),
+								   1);
 }
 
 #if defined(CONFIG_NFC_TAG_CH_REQUESTER)
@@ -582,33 +660,36 @@ static int tnep_ch_request_prepare(void)
 }
 
 static int tnep_ch_select_received(const struct nfc_tnep_ch_record *ch_select,
-				   bool inactive)
+								   bool inactive)
 {
 	int err;
 	const struct nfc_ndef_record_desc *oob_data = NULL;
 
-	if (!ch_select->count) {
+	if (!ch_select->count)
+	{
 		return -EINVAL;
 	}
 
 	/* All alternative carrier are inactive */
-	if (inactive) {
+	if (inactive)
+	{
 		/* Try send request again. */
 		return carrier_prepare();
 	}
 
 	err = check_oob_carrier(ch_select, &oob_data);
-	if (err) {
+	if (err)
+	{
 		return err;
 	}
 
 	err = oob_le_data_handle(oob_data, false);
-	if (err) {
+	if (err)
+	{
 		return err;
 	}
 
 	return 0;
-
 }
 #endif /* defined(CONFIG_NFC_TAG_CH_REQUESTER) */
 
@@ -617,19 +698,22 @@ static int tnep_ch_request_received(const struct nfc_tnep_ch_request *ch_req)
 	int err;
 	const struct nfc_ndef_record_desc *oob_data = NULL;
 
-	if (!ch_req->ch_record.count) {
+	if (!ch_req->ch_record.count)
+	{
 		return -EINVAL;
 	}
 
 	err = check_oob_carrier(&ch_req->ch_record, &oob_data);
-	if (err) {
+	if (err)
+	{
 		return err;
 	}
 
 	bt_le_adv_stop();
 
 	err = oob_le_data_handle(oob_data, true);
-	if (err) {
+	if (err)
+	{
 		return err;
 	}
 
@@ -641,8 +725,7 @@ static struct nfc_tnep_ch_cb ch_cb = {
 	.request_msg_prepare = tnep_ch_request_prepare,
 	.select_msg_recv = tnep_ch_select_received,
 #endif
-	.request_msg_recv = tnep_ch_request_received
-};
+	.request_msg_recv = tnep_ch_request_received};
 /** .. include_endpoint_nfc_tnep_ch_tag_rst */
 
 static void nfc_init(void)
@@ -651,40 +734,46 @@ static void nfc_init(void)
 
 	/* TNEP init */
 	err = nfc_tnep_tag_tx_msg_buffer_register(tnep_buffer, tnep_swap_buffer,
-						  sizeof(tnep_buffer));
-	if (err) {
+											  sizeof(tnep_buffer));
+	if (err)
+	{
 		printk("Cannot register tnep buffer, err: %d\n", err);
 		return;
 	}
 
 	err = nfc_tnep_tag_init(events, NFC_TNEP_EVENTS_NUMBER,
-				nfc_t4t_ndef_rwpayload_set);
-	if (err) {
+							nfc_t4t_ndef_rwpayload_set);
+	if (err)
+	{
 		printk("Cannot initialize TNEP protocol, err: %d\n", err);
 		return;
 	}
 
 	/* Set up NFC */
 	err = nfc_t4t_setup(nfc_callback, NULL);
-	if (err) {
+	if (err)
+	{
 		printk("Cannot setup NFC T4T library!\n");
 		return;
 	}
 
 	err = nfc_tnep_tag_initial_msg_create(2, tnep_initial_msg_encode);
-	if (err) {
+	if (err)
+	{
 		printk("Cannot create initial TNEP message, err: %d\n", err);
 	}
 
 	err = nfc_tnep_ch_service_init(&ch_cb);
-	if (err) {
+	if (err)
+	{
 		printk("TNEP CH Service init error: %d\n", err);
 		return;
 	}
 
 	/* Start sensing NFC field */
 	err = nfc_t4t_emulation_start();
-	if (err) {
+	if (err)
+	{
 		printk("Cannot start emulation!\n");
 		return;
 	}
@@ -697,11 +786,15 @@ void button_changed(uint32_t button_state, uint32_t has_changed)
 	int err;
 	uint32_t buttons = button_state & has_changed;
 
-	if (buttons & KEY_BOND_REMOVE_MASK) {
+	if (buttons & KEY_BOND_REMOVE_MASK)
+	{
 		err = bt_unpair(BT_ID_DEFAULT, NULL);
-		if (err) {
+		if (err)
+		{
 			printk("Bond remove failed err: %d\n", err);
-		} else {
+		}
+		else
+		{
 			printk("All bond removed\n");
 		}
 	}
@@ -715,43 +808,50 @@ void main(void)
 
 	/* Configure LED-pins as outputs */
 	err = dk_leds_init();
-	if (err) {
+	if (err)
+	{
 		printk("Cannot init LEDs!\n");
 		return;
 	}
 
 	/* Configure buttons */
 	err = dk_buttons_init(button_changed);
-	if (err) {
+	if (err)
+	{
 		printk("Cannot init buttons (err %d\n", err);
 	}
 
 	err = bt_conn_auth_cb_register(&conn_auth_callbacks);
-	if (err) {
+	if (err)
+	{
 		printk("Failed to register authorization callbacks.\n");
 		return;
 	}
 
 	err = bt_conn_auth_info_cb_register(&conn_auth_info_callbacks);
-	if (err) {
+	if (err)
+	{
 		printk("Failed to register authorization info callbacks.\n");
 		return;
 	}
 
 	err = bt_enable(NULL);
-	if (err) {
+	if (err)
+	{
 		printk("Bluetooth init failed (err %d)\n", err);
 		return;
 	}
 
 	printk("Bluetooth initialized\n");
 
-	if (IS_ENABLED(CONFIG_SETTINGS)) {
+	if (IS_ENABLED(CONFIG_SETTINGS))
+	{
 		settings_load();
 	}
 
 	err = paring_key_generate();
-	if (err) {
+	if (err)
+	{
 		return;
 	}
 
@@ -759,7 +859,8 @@ void main(void)
 	pair_key_generate_init();
 	nfc_init();
 
-	for (;;) {
+	for (;;)
+	{
 		k_poll(events, ARRAY_SIZE(events), K_FOREVER);
 		nfc_tnep_tag_process();
 		paring_key_process();
